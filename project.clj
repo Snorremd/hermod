c896a6392bf31e103fbb71d510122db145cf5ead
Snorre Magnus Davøen <snorremd@gmail.com>
2016-02-28 21:21:07 +0100

    Add code climate config and fix README project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hermod "0.1.0-SNAPSHOT"
   :description "Hermóðr sends a message to Hipchat/Slack when running lein deploy."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/snorremd/hermod"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true

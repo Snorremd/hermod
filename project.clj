b0287e54182be724a096b79148679006e2a65aea
Snorre Magnus Davøen <snorremd@gmail.com>
2016-03-06 18:54:29 +0100

    Rename project to ratatoskr
    Herod already existed as a messaging library in the Clojure sphere.
    Renamed the project to Ratatoskr (after the squirrel delivering
    messages between the top and bottom of Ygraddsil) which does not seem
    to be used in the Clojure ecosystem (as far as googling reveals).

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hermod "0.1.0-SNAPSHOT"
-  :description "Hermóðr sends a message to Hipchat/Slack when running lein deploy."
-  :url "https://github.com/snorremd/hermod"
+  :description "[DEPRECATED] - Hermóðr renamed Ratatoskr as Hermod is used by https://clojars.org/com.tbaldridge.hermod in the clojure sphere."
+  :url "https://github.com/snorremd/ratatoskr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
